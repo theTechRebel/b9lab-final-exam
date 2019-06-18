3abe1d74ac93db8657ec4e17512cac60bdd18bf8
thetechrebel <stevedumbura@gmail.com>
2019-06-18 23:58:44 +0200

    fixed migration

--- migrations/2_regulator.js
+++ migrations/2_regulator.js
@@ -1,12 +1,18 @@
 const Regulator = artifacts.require("Regulator");
-const TollBoothOperator = artifacts.require("TollBoothOperator");
-
-
 module.exports = async function(deployer) {
-    deployer.deploy(Regulator).then(()=>{
-        return deployer.deploy(TollBoothOperator,false,100,Regulator.address);
-    }).then(async()=>{
-        var regulator = await Regulator.deployed();
-        await regulator.createNewOperator(TollBoothOperator.address,100);
-    })
+    const accounts = await web3.eth.getAccounts();
+    if(accounts.length == 0 ){
+        throw new Error("No account with which to transact");
+    }else{
+        const regulatorcontract = await deployer.deploy(Regulator);
+        const txobj = await regulatorcontract.createNewOperator(accounts[1],100);
+        const logNewOperator = txobj.logs[0];
+        if(logNewOperator.event == "LogTollBoothOperatorCreated"){
+            const check = await regulatorcontract.isPaused.call();
+            if(check){
+                await regulatorcontract.setPaused(false,{from:accounts[0]});
+            }
+        }
+        
+    }
 };

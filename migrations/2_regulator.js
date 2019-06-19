2b43c504f4e91e2558026af27a461549ad38d0c6
thetechrebel <stevedumbura@gmail.com>
2019-06-19 02:31:56 +0200

    fixed migration

--- migrations/2_regulator.js
+++ migrations/2_regulator.js
@@ -1,18 +1,20 @@
 const Regulator = artifacts.require("Regulator");
+const TollBoothOperator = artifacts.require("TollBoothOperator");
 module.exports = async function(deployer) {
-    const accounts = await web3.eth.getAccounts();
-    if(accounts.length == 0 ){
-        throw new Error("No account with which to transact");
-    }else{
+        const accounts = await web3.eth.getAccounts();
         const regulatorcontract = await deployer.deploy(Regulator);
         const txobj = await regulatorcontract.createNewOperator(accounts[1],100);
-        const logNewOperator = txobj.logs[0];
+        const logNewOperator = txobj.logs[1];
         if(logNewOperator.event == "LogTollBoothOperatorCreated"){
-            const check = await regulatorcontract.isPaused.call();
-            if(check){
-                await regulatorcontract.setPaused(false,{from:accounts[0]});
+            const address = logNewOperator.args[1];
+            const instane = await TollBoothOperator.at(address);
+            const pause = await instane.isPaused();
+            if(pause){
+                const result=await instane.setPaused(false,{from:accounts[1]});
+                console.log(result);
             }
+        }else{
+            console.log(txobj);
+            throw new Error("No contract ws created");
         }
-        
     }
-};
